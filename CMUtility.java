f41d806fd8d530167cc76e2cd6a7195a27d25962
Wang961506 <56723331+Wang961506@users.noreply.github.com>
2020-01-21 20:51:02 +0000

    added the function to detect and record the number of mines around and keyboard input characters and numbers

--- CMUtility.java
+++ CMUtility.java
@@ -1,5 +1,88 @@
 package wangzhe.teamproject;
 
-public class CMUtility {
+import java.util.*;
 
+public class CMUtility {
+    private static Scanner scanner = new Scanner(System.in);
+    /**
+	用于界面菜单的选择。该方法读取键盘，如果用户键入’1’-’3’中的任意字符，则方法返回。返回值为用户键入字符。
+	*/
+	public static char readMenuSelection() {
+        char c;
+        for (; ; ) {
+            String str = readKeyBoard(1, false);
+            c = str.charAt(0);
+            if (c != '1' && c != '2' && 
+                c != '3' ) {
+                System.out.print("选择错误，请重新输入：");
+            } 
+            else break;
+        }
+        return c;
+    }
+
+	/**
+	从键盘读取一个字符，将其作为方法的返回值。
+	*/
+    public static char readChar() {
+        String str = readKeyBoard(1, false);
+        return str.charAt(0);
+    }
+
+	/**
+	从键盘读取一个长度不超过1位的整数，并将其作为方法的返回值。
+	如果用户不输入字符而直接回车，方法将以defaultValue 作为返回值。
+	*/
+    public static int readInt() {
+        int n;
+        for (; ; ) {
+            String str = readKeyBoard(1, true);
+            try {
+                n = Integer.parseInt(str);
+                break;
+            } catch (NumberFormatException e) {
+                System.out.print("数字输入错误，请重新输入：");
+            }
+        }
+        return n;
+    }
+    
+	/**
+	用于确认选择的输入。该方法从键盘读取‘Y’或’N’，并将其作为方法的返回值。
+	*/
+    public static char readConfirmSelection() {
+        char c;
+        for (; ; ) {
+            String str = readKeyBoard(1, false).toUpperCase();
+            c = str.charAt(0);
+            if (c == 'Y' || c == 'N') {
+                break;
+            } else {
+                System.out.print("选择错误，请重新输入：");
+            }
+        }
+        return c;
+    }
+
+    private static String readKeyBoard(int limit, boolean blankReturn) {
+        String line = "";
+
+        while (scanner.hasNextLine()) {
+            line = scanner.nextLine();
+            if (line.length() == 0) {
+                if (blankReturn) return line;
+                else continue;
+            }
+
+            if (line.length() < 1 || line.length() > limit) {
+                System.out.print("输入长度（不大于" + limit + "）错误，请重新输入：");
+                continue;
+            }
+            break;
+        }
+
+        return line;
+    }
 }
+
+
--- Method_wangzhe.java
+++ Method_wangzhe.java
@@ -2,4 +2,90 @@ package wangzhe.teamproject;
 
 public class Method_wangzhe {
 
-}
+		private static Grid[][] grid = new Grid[10][10];
+		private static int numOfBomb = 10;
+		
+		/**
+		 * 炸弹安放好后进行每一个点的周围炸弹数记录，NumberofBomb是每个不是炸弹的点的四周炸弹个数
+		 */
+		public void GetNumber() {
+			int[][] offset = { { -1, -1 }, { 0, -1 }, { 1, -1 }, { 1, 0 }, { 1, 1 }, { 0, 1 }, { -1, 1 }, { -1, 0 } };
+			int bombCount = 0;
+			for (int i = 0; i < grid.length; ++i) {
+				for (int j = 0; j < grid[i].length; ++j) {
+					// 如果是炸弹，不写数字上去
+					if (grid[i][j].getContent() == '*') {
+						continue;
+					}
+					// 如果不是炸弹，遍历周边的8个方块，标记总数在此方块上
+					for (int[] off : offset) {
+						int row = i + off[1];
+						int col = j + off[0];
+						if (verify(row, col) && grid[row][col].getContent() == '*') {
+							bombCount++;
+						}
+					} // 如果炸弹的个数不为0,标注出来
+					if (bombCount > 0) {
+						grid[i][j].getNumberofBomb(bombCount);
+					}
+					
+				}
+			}
+		}
+	//判断位置是否越界
+		private boolean verify(int row, int col) {
+
+			return row >= 0 && row < this.grid.length && col >= 0 && col < this.grid[1].length;
+
+		}
+
+	}
+
+	class Grid {
+		// Use as an instance model for each square
+		private char content;
+		private boolean state;// It describes the state of the square
+		private int row;
+		private int column;
+		private int NumberofBomb;
+
+		public char getContent() {
+			return content;
+		}
+
+		public void setContent(char content) {
+			this.content = content;
+		}
+
+		public boolean isState() {
+			return state;
+		}
+
+		public void setState(boolean state) {
+			this.state = state;
+		}
+
+		public int getRow() {
+			return row;
+		}
+
+		public void setRow(int row) {
+			this.row = row;
+		}
+
+		public int getColumn() {
+			return column;
+		}
+
+		public void setColumn(int column) {
+			this.column = column;
+		}
+		
+		public int getNumberofBomb(int number)
+		{
+			NumberofBomb=number;
+			return NumberofBomb;
+		}
+
+	}
+
