d6b48dcbdb8346054520aa990aa2daa6561391c9
Wang961506 <56723331+Wang961506@users.noreply.github.com>
2020-01-22 20:52:24 +0000

    try to write the click box to judge the function

--- Method_wangzhe.java
+++ Method_wangzhe.java
@@ -6,25 +6,26 @@ public class Method_wangzhe {
 		private static int numOfBomb = 10;
 		
 		/**
-		 * 炸弹安放好后进行每一个点的周围炸弹数记录，NumberofBomb是每个不是炸弹的点的四周炸弹个数
+		 * After the bomb is placed, the number of bombs around each point is recorded. 
+		 * Numberof bombs is the number of bombs around each point that is not a bomb.
 		 */
 		public void GetNumber() {
 			int[][] offset = { { -1, -1 }, { 0, -1 }, { 1, -1 }, { 1, 0 }, { 1, 1 }, { 0, 1 }, { -1, 1 }, { -1, 0 } };
 			int bombCount = 0;
 			for (int i = 0; i < grid.length; ++i) {
 				for (int j = 0; j < grid[i].length; ++j) {
-					// 如果是炸弹，不写数字上去
+					// If it's a bomb, don't put Numbers on it
 					if (grid[i][j].getContent() == '*') {
 						continue;
 					}
-					// 如果不是炸弹，遍历周边的8个方块，标记总数在此方块上
+					// If it is not a bomb, traverse the 8 squares around it, marking the total on the square
 					for (int[] off : offset) {
 						int row = i + off[1];
 						int col = j + off[0];
 						if (verify(row, col) && grid[row][col].getContent() == '*') {
 							bombCount++;
 						}
-					} // 如果炸弹的个数不为0,标注出来
+					} // If the number of bombs is not zero, label it
 					if (bombCount > 0) {
 						grid[i][j].getNumberofBomb(bombCount);
 					}
@@ -32,7 +33,67 @@ public class Method_wangzhe {
 				}
 			}
 		}
-	//判断位置是否越界
+		
+		public int pailei(int x, int y){
+			if(this.grid[x][y].getNumberofBomb()==-1){
+				blust();
+				return -1;//Step on a landmine
+			}else if(this.grid[x][y].getNumberofBomb()>0){//There are bombs around.
+				this.grid[x][y].setContent((char)(this.grid[x][y].getNumberofBomb()+'0'));
+			}
+			else//No bombs around.
+			{
+				this.grid[x][y].setContent(' ');
+				int isok=clickBlank(x,y);//Call each other
+				if(isok==1 || isok==0)
+					return 1;
+				
+					
+					
+			}
+			show();
+			return 1;//No landmines
+		}
+		
+		//If it's blank, it's going to determine if there are Numbers or bombs around, 
+		//and if there are, it's going to terminate the call to pailei.
+		public int clickBlank(int x,int y) {
+			boolean isok=true;
+			while(isok)
+			{
+			int rowofdirection=(int)(Math.random()*4);
+			if(rowofdirection == 0)
+			if(verify(x-1,y)&&grid[x-1][y].getNumberofBomb()!=-1) {
+				int i=pailei(x-1,y);
+				if(i==-1 || i==1)
+					return 1;
+			}
+			if(rowofdirection == 1)
+			if(verify(x+1,y)&&grid[x+1][y].getNumberofBomb()!=-1) {
+				pailei(x+1,y);
+				int i=pailei(x+1,y);
+				if(i==-1 || i==1)
+					return 1;
+			}
+			if(rowofdirection == 2)
+			if(verify(x,y-1)&&grid[x][y-1].getNumberofBomb()!=-1) {
+				pailei(x,y-1);
+				int i=pailei(x,y-1);
+				if(i==-1 || i==1)
+					return 1;
+			}
+			if(rowofdirection == 3)
+			if(verify(x,y+1)&&grid[x][y+1].getNumberofBomb()!=-1) {
+				pailei(x,y+1);
+				int i=pailei(x,y+1);
+				if(i==-1 || i==1)
+					return 1;
+			}
+			}
+		 return -1;//Out of bounds returns -1
+		}
+			
+	//Determine if the position is out of bounds
 		private boolean verify(int row, int col) {
 
 			return row >= 0 && row < this.grid.length && col >= 0 && col < this.grid[1].length;
@@ -86,6 +147,15 @@ public class Method_wangzhe {
 			NumberofBomb=number;
 			return NumberofBomb;
 		}
+
+		public int getNumberofBomb() {
+			return NumberofBomb;
+		}
+
+		public void setNumberofBomb(int numberofBomb) {
+			NumberofBomb = numberofBomb;
+		}
+		
 
 	}
 
