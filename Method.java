66a97e48bccf2c60eaf15d1dbf6eee26405730be
tom_glb <goodMorning_glb@atguigu.com>
2020-01-24 06:48:21 +0800

    second commit

--- Method.java
+++ Method.java
@@ -7,28 +7,39 @@ public class Method {
 	private  static int numOfBomb = 10;
 
 	public void startjudge() {
-		while (true) {
-		   Show();//显示棋盘
-		   System.out.print("请您操作 C代表选行列，M代表标雷 :");
-		   Scanner san = new Scanner(System.in);
-		   CMUtility std = new CMUtility();//调用
-		   char command = std.readChar();
-		   if (command == 'C') {
-			   System.out.print("Select the Row:");
-			   int x = std.readInt();
-			   System.out.print("Select the Column:");
-			   int y = std.readInt();
-			   boolean flag =;//标志是否踩到雷
-			   if (!flag) {
-				 
-				   Show();//调用显示函数
-				   System.out.println("Game Over");
-				   break;
-			   }
-		   } else if (command == 'M') {
-		   System.out.print("Select the Row:");
-		   }
-		}
+		  while (true) {
+			  Show();
+			  System.out.print("请您操作 C代表选行列，M代表标雷 :");
+			  Scanner san = new Scanner(System.in);
+			  CMUtility std = new CMUtility();
+			  char command = std.readChar();
+			  if (command == 'C') {
+				  System.out.print("Select the Row:");
+				  int x = std.readInt();
+				  System.out.print("Select the Column:");
+				  int y = std.readInt();
+				  boolean flag = this.Click(x, y);
+				  if (!flag) {
+					  ShowAll();
+					  Show();
+					  System.out.println("Game Over");
+					  break;
+				  }
+			  } else if (command == 'M') {
+				  System.out.print("Select the Row:");
+				  int x = std.readInt();
+				  System.out.print("Select the Column:");
+				  int y = std.readInt();
+				  this.Mark(x, y);
+			  } else
+				  System.out.println("error");
+			  if (UnOpened() == 10) {
+				  ShowAll();
+				  Show();
+				  System.out.println("Successful");
+				  break;
+			  }
+		  }
 	}
 	
 	//显示未开盒子的数量
